cae9cd17f078ac3f0b5ad65df747389b17e70e8e
Samuel Martín <s.martin.mor27@gmail.com>
2023-09-15 16:42:02 -0600

    Proyecto inicial

---
+++ Proyecto BD and Git/AccesoDatos/AccesoDatosProductos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos
+{
+    public class AccesoDatosProductos
+    {
+        Base b = new Base();
+        public string Borrar(dynamic Entidad)
+        {
+            return b.Comando(string.Format("delete from productos where idproducto={0}", Entidad._Idproductos));
+        }
+
+        public string Guardar(dynamic Entidad)
+        {
+            return b.Comando(string.Format("insert into productos values(null,'{0}','{1}',{2})",
+            Entidad._Nombre, Entidad._Descripcion, Entidad._Precio));
+        }
+
+        public void Modificar(dynamic Entidad)
+        {
+            b.Comando(string.Format("update productos set nommbre='{0}',descripcion='{1}', precio={2}, where idproducto={3}",
+            Entidad._Nombre, Entidad._Descripcion, Entidad._Precio, Entidad._Idproductos));
+        }
+
+        public DataSet Mostrar(string f)
+        {
+            return b.Mostrar(string.Format("select * from productos where nommbre like '%{0}%'", f), "productos");
+        }
+    }
+}
---
+++ Proyecto BD and Git/AccesoDatos/Tfunciones.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos
+{   public interface TFunciones
+    {
+        string Guardar(dynamic Entidad);
+        DataSet Mostrar(string f);
+        string Borrar(dynamic Entidad);
+        void Modificar(dynamic Entidad);
+    }
+}
